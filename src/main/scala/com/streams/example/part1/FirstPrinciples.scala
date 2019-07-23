30fc58121bacbf2c3363835e890e78bb4c656e4b
Uladzislau Kiyeuski <uladzislau.kiyeuski@havi.com>
2019-07-23 12:57:20 +0300

    add more examples

--- src/main/scala/com/streams/example/part1/FirstPrinciples.scala
+++ src/main/scala/com/streams/example/part1/FirstPrinciples.scala
@@ -1,8 +1,9 @@
 package com.streams.example.part1
 
+import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.stream.scaladsl.{Flow, RunnableGraph, Sink, Source}
 
 import scala.concurrent.Future
 
@@ -19,4 +20,66 @@ object FirstPrinciples extends App {
   val graph = source.to(sink)
 
   graph.run()
+
+  // flows transform elements
+  val flow = Flow[Int].map(x => x + 1)
+  val sourceWithFlow = source.via(flow)
+  val flowWithSink = flow.to(sink)
+
+//  sourceWithFlow.to(sink).run()
+//  source.to(flowWithSink).run()
+//  source.via(flow).to(sink).run()
+
+// nulls are not allowed
+//  val illegalSource = Source.single[String](null)
+//  illegalSource.to(Sink.foreach(println)).run()
+  //  use Options instead
+
+  // various kinds of sources
+  val finiteSource = Source.single(1)
+  val anotherFiniteSource = Source(List(1, 2, 3))
+  val emptySource = Source.empty[Int]
+  val infiniteSource = Source(Stream.from(1)) // do not confuse an Akka stream with a "collection" Stream
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+  val futureSource = Source.fromFuture(Future(42))
+
+  // sinks
+  val theMostBoringSink = Sink.ignore
+  val foreachSink = Sink.foreach[String](println)
+  val headSink = Sink.head[Int] // retrieves head and then closes the stream
+  val foldSink = Sink.fold[Int, Int](0)((a, b) => a + b)
+
+  // flows - usually mapped to collection operators
+  val mapFlow = Flow[Int].map(x => 2 * x)
+  val takeFlow = Flow[Int].take(5)
+  // drop, filter
+  // NOT have flatMap
+
+  // source -> flow -> flow -> ... -> sink
+  val doubleFlowGraph = source.via(mapFlow).via(takeFlow).to(sink)
+  //  doubleFlowGraph.run()
+
+  // syntactic sugars
+  val mapSource = Source(1 to 10).map(x => x * 2) // Source(1 to 10).via(Flow[Int].map(x => x * 2))
+  // run streams directly
+  //  mapSource.runForeach(println) // mapSource.to(Sink.foreach[Int](println)).run()
+
+
+  /**
+    * Exercise: create a stream that takes the names of persons, then you will keep the first 2 names with length > 5 characters.
+    *
+    */
+
+  val names = List("Alice", "Bob", "Charlie", "David", "Martin", "AkkaStreams")
+
+  val source1 = Source(names)
+  val filterFlow = Flow[String].filter(_.length > 5)
+  val reduceFlow = Flow[String].take(2)
+  val sink1 = Sink.foreach[String](println)
+
+  val resultGraph = source1.via(filterFlow).via(reduceFlow).to(sink1)
+  resultGraph.run()
+  source1.filter(_.length > 5).take(2).runForeach(println)
+
 }
