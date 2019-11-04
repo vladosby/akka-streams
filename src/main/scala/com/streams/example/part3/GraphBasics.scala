23d00bb03b99646967a09cca90e6b354140f3bd9
Uladzislau Kiyeuski <uladzislau.kiyeuski@havi.com>
2019-11-04 23:18:33 +0300

    Create simple graph that increment and multiply one stream and prints two results

---
+++ src/main/scala/com/streams/example/part3/GraphBasics.scala
@@ -0,0 +1,38 @@
+package com.streams.example.part3
+
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, ClosedShape}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, RunnableGraph, Sink, Source, Zip}
+
+object GraphBasics extends App {
+
+  implicit val system = ActorSystem("GraphBasics")
+  implicit val materializer = ActorMaterializer()
+
+  val input = Source(1 to 1000)
+  val increment = Flow[Int].map(_ + 1)
+  val multiplier = Flow[Int].map(_ * 10)
+  val output = Sink.foreach[(Int, Int)](println)
+
+  val graph = RunnableGraph.fromGraph(
+
+    GraphDSL.create() {implicit builder: GraphDSL.Builder[NotUsed] =>
+    import GraphDSL.Implicits._
+
+    val broadcast = builder.add(Broadcast[Int](2))
+    val zip = builder.add(Zip[Int, Int])
+
+    input ~> broadcast
+
+    broadcast.out(0) ~> increment ~> zip.in0
+    broadcast.out(1) ~> multiplier ~> zip.in1
+
+    zip.out ~> output
+
+    ClosedShape
+  })
+
+  graph.run()
+
+}
